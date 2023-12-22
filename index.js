d791348e3255380f8546989829bf8c965e0d4d74
mahedi205339 <mahedi205339gmaol.com>
2023-12-22 20:25:48 +0600

    added updete task route

--- index.js
+++ index.js
@@ -9,63 +9,131 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// task-manager
-// nR90Pml0DqBGGDm6
+
 
 const client = new MongoClient(process.env.DB_URI, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  })
-  async function run() {
-    try {
-  
-      const tasksCollection = client.db('Task-management').collection('tasks')
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+async function run() {
+  try {
 
-      app.post('/tasks', async(req,res)=>{
-        const tasks = req.body ;
-        const result = await tasksCollection.insertOne(tasks)
-        res.send(result)
-      })
-      
-      app.get('/tasks/:email',async(req,res)=>{
-        const email = req.params.email;
-        const query = {email: email}
-        const result = await tasksCollection.find(query).toArray()
-        res.send(result)
-      })
+    const tasksCollection = client.db('Task-management').collection('tasks')
 
-      app.patch('/task/:id', async (req, res) => {
+    app.post('/tasks', async (req, res) => {
+      const tasks = req.body;
+      const result = await tasksCollection.insertOne(tasks)
+      res.send(result)
+    })
+    app.get('/tasks', async (req, res) => {
+      const result = await tasksCollection.find().toArray()
+      res.send(result)
+    })
+
+
+    app.get('/tasks/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await tasksCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+
+    app.get('/tasks/ongoing/:id', async (req, res) => {
+      try {
         const id = req.params.id;
-        const filter = { _id: new ObjectId(id) };
-        const updatedDoc = {
-          $set: {
-            status: 'ongoing'
-          }
+        const query = { _id: new ObjectId(id) }
+        const result = await tasksCollection.findOne(query);
+        res.send(result);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    })
+
+
+
+    app.patch('/task/ongoing/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: 'ongoing'
         }
-        const result = await tasksCollection.updateOne(filter, updatedDoc)
-        res.send(result)
-      })
-  
-      // Send a ping to confirm a successful connection
-      await client.db('admin').command({ ping: 1 })
-      console.log(
-        'Pinged your deployment. You successfully connected to MongoDB!'
-      )
-    } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
+      }
+      const result = await tasksCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+    app.patch('/task/complete/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: 'complete'
+        }
+      }
+      const result = await tasksCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+    app.patch('/task/todo/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: 'todo'
+        }
+      }
+      const result = await tasksCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+
+
+    app.patch('/tasks/:id', async (req, res) => {
+      try {
+          const task = req.body;
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) }
+          const updatedDoc = {
+              $set: {
+                  name: task.name,
+                  email: task.email,
+                  priority: task.priority,
+                  title: task.title,
+                  taskDescription: task.taskDescription
+              }
+          }
+          const result = await tasksCollection.updateOne(filter, updatedDoc)
+          res.send(result);
+      } catch {
+          error => console.log(error)
+      }
+  })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
-  run().catch(console.dir)
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from task-manager Server..')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Task-Manager is running on port ${port}`)
-  })
+  res.send('Hello from task-manager Server..')
+})
+
+app.listen(port, () => {
+  console.log(`Task-Manager is running on port ${port}`)
+})
